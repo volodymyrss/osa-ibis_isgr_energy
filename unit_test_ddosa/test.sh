11d760b1d5acdfcfb5ee692d68f68c08d7f51986
Volodymyr Savchenko <savchenk@ccage007.in2p3.fr>
2017-01-19 01:01:03 +0100

    test

--- unit_test_ddosa/test.sh
+++ unit_test_ddosa/test.sh
@@ -4,4 +4,5 @@ TMPDIR=$PWD/tmp
     mkdir -pv $TMPDIR
     cd $TMPDIR
     rundda.py ibis_isgr_energy -m ddosa  -m /onlybright/v4.2 -a 'ddosa.ScWData(input_scwid="023900320010.001")'
+ #   rundda.py BinBackgroundSpectrum -m ddosa -m /eddosa/GG5  -m /onlybright/v4.2 -a 'ddosa.ScWData(input_scwid="023900320010.001")' -f BinBackgroundSpectrum
 )
